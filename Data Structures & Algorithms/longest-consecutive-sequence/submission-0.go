59d1e5eee23d1bb90a8fb15005eec39f6962ea90
Andrea Trevisin <andre.trevisin@gmail.com>
2026-06-19 16:16:19 +0200

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.go
@@ -0,0 +1,25 @@
+func longestConsecutive(nums []int) int {
+	set := make(map[int]int)
+	for _, n := range nums {
+		set[n] = n
+	}
+	
+	maxlen := 0
+	for i := 0; i < len(nums); i++ {
+		if _, ok := set[nums[i]-1]; !ok {
+			seq := make([]int, 0 , len(nums))
+			next := nums[i]
+			for {
+				v, ok := set[next]
+				if !ok {
+					break
+				}
+				seq = append(seq, v)
+				next++
+			}
+			maxlen = max(maxlen, len(seq))
+			print(seq)
+		}
+	}
+	return maxlen
+}
